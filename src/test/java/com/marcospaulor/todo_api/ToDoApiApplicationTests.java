f57535eeb1cbde9235006f180a1eab74a3436543
marcospaulor <dev.silva.marcos@gmail.com>
2025-04-26 14:28:45 -0300

    real first commit

---
+++ src/test/java/com/marcospaulor/todo_api/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcospaulor.todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
